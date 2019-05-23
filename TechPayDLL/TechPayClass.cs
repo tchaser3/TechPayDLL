70002b629982b6734cdd1b6e90a3597d06d5f90c
Terry Holmes <tchaser3@outlook.com>
2019-05-23 15:12:33 -0400

    Add Project Tech Pay to the DLL

--- TechPayDLL/TechPayClass.cs
+++ TechPayDLL/TechPayClass.cs
@@ -36,6 +36,148 @@ namespace TechPayDLL
         FindTechPayItemByDescriptionDataSet aFindTechPayItemByDescriptionDataSet;
         FindTechPayItemByDescriptionDataSetTableAdapters.FindTechPayItemByDescriptionTableAdapter aFindTechPayItemByDescriptionTableAdapter;
 
+        ProjectTechPayItemsDataSet aProjectTechPayItemsDataSet;
+        ProjectTechPayItemsDataSetTableAdapters.projecttechpayitemsTableAdapter aProjectTechPayItemsTableAdapter;
+
+        InsertProjectTechPayItemEntryTableAdapters.QueriesTableAdapter aInsertProjectTechPayItemTableAdapter;
+
+        UpdateProjectTechPayItemEntryTableAdapters.QueriesTableAdapter aUpdateProjectTechPayItemTableAdapter;
+
+        FindProjectTechPayByProjectIDDataSet aFindProjectTechPayByProjectIDDataSet;
+        FindProjectTechPayByProjectIDDataSetTableAdapters.FindProjectTechPaybyProjectIDTableAdapter aFindProjectTechPayByProjectIDTableAdapter;
+
+        FindProjectTechPayTotalsDataSet aFindProjectTechPayTotalsDataSet;
+        FindProjectTechPayTotalsDataSetTableAdapters.FindProjectTechPayTotalsTableAdapter aFindProjectTechpayTotalsTableAdapter;
+
+        FindEmployeeTechPayByDateRangeDataSet aFindEmployeeTechPayByDateRangeDataSet;
+        FindEmployeeTechPayByDateRangeDataSetTableAdapters.FindEmployeeTechPayByDateRangeTableAdapter aFindEmployeeTechPayByDateRangeTableAdapter;
+
+        FindManagerEmployeeTechPayDataSet aFindManagerEmployeeTechPayDataSet;
+        FindManagerEmployeeTechPayDataSetTableAdapters.FindManagerEmployeeTechPayTableAdapter aFindManagerEmployeeTechPayTableAdapter;
+
+        public FindManagerEmployeeTechPayDataSet FindManagerEmployeeTechPay(int intManagerID, DateTime datStartDate, DateTime datEndDate)
+        {
+            try
+            {
+                aFindManagerEmployeeTechPayDataSet = new FindManagerEmployeeTechPayDataSet();
+                aFindManagerEmployeeTechPayTableAdapter = new FindManagerEmployeeTechPayDataSetTableAdapters.FindManagerEmployeeTechPayTableAdapter();
+                aFindManagerEmployeeTechPayTableAdapter.Fill(aFindManagerEmployeeTechPayDataSet.FindManagerEmployeeTechPay, intManagerID, datStartDate, datEndDate);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Find Manager Employee Tech Pay " + Ex.Message);
+            }
+
+            return aFindManagerEmployeeTechPayDataSet;
+        }
+        public FindEmployeeTechPayByDateRangeDataSet FindEmployeeTechPayByDateRange(int intEmployeeID, DateTime datStartDate, DateTime datEndDate)
+        {
+            try
+            {
+                aFindEmployeeTechPayByDateRangeDataSet = new FindEmployeeTechPayByDateRangeDataSet();
+                aFindEmployeeTechPayByDateRangeTableAdapter = new FindEmployeeTechPayByDateRangeDataSetTableAdapters.FindEmployeeTechPayByDateRangeTableAdapter();
+                aFindEmployeeTechPayByDateRangeTableAdapter.Fill(aFindEmployeeTechPayByDateRangeDataSet.FindEmployeeTechPayByDateRange, intEmployeeID, datStartDate, datEndDate);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Find Employee Tech Pay By Date Range " + Ex.Message);
+            }
+
+            return aFindEmployeeTechPayByDateRangeDataSet;
+        }
+        public FindProjectTechPayTotalsDataSet FindProjectTechPayTotals(int intProjectID, bool blnIsConstruction)
+        {
+            try
+            {
+                aFindProjectTechPayTotalsDataSet = new FindProjectTechPayTotalsDataSet();
+                aFindProjectTechpayTotalsTableAdapter = new FindProjectTechPayTotalsDataSetTableAdapters.FindProjectTechPayTotalsTableAdapter();
+                aFindProjectTechpayTotalsTableAdapter.Fill(aFindProjectTechPayTotalsDataSet.FindProjectTechPayTotals, intProjectID, blnIsConstruction);
+            }
+            catch (Exception ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Find Project Tech Pay Total " + ex.Message);
+            }
+
+            return aFindProjectTechPayTotalsDataSet;
+        }
+        public FindProjectTechPayByProjectIDDataSet FindProjectTechPayByProjectID(int intProjectID, bool blnIsConstruction)
+        {
+            try
+            {
+                aFindProjectTechPayByProjectIDDataSet = new FindProjectTechPayByProjectIDDataSet();
+                aFindProjectTechPayByProjectIDTableAdapter = new FindProjectTechPayByProjectIDDataSetTableAdapters.FindProjectTechPaybyProjectIDTableAdapter();
+                aFindProjectTechPayByProjectIDTableAdapter.Fill(aFindProjectTechPayByProjectIDDataSet.FindProjectTechPaybyProjectID, intProjectID, blnIsConstruction);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Find Project Tech Pay By Project ID " + Ex.Message);
+            }
+
+            return aFindProjectTechPayByProjectIDDataSet;
+        }
+        public bool UpdateProjectTechPayItem(int intTransactionID, int intQuantity, decimal decTechPayPrice, decimal decTotalTechPayPrice)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateProjectTechPayItemTableAdapter = new UpdateProjectTechPayItemEntryTableAdapters.QueriesTableAdapter();
+                aUpdateProjectTechPayItemTableAdapter.UpdateProjectTechPayItem(intTransactionID, intQuantity, decTechPayPrice, decTotalTechPayPrice);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Update Project Tech Pay Item " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool InsertProjectTechpayItem(int intProjectID, bool blnIsConstruction, string strProjectType, int intEmployeeID, int intWarehouseID, int intTechPayID, decimal decTechPayPrice, int intQuantity, decimal decTotalTechPayPrice)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aInsertProjectTechPayItemTableAdapter = new InsertProjectTechPayItemEntryTableAdapters.QueriesTableAdapter();
+                aInsertProjectTechPayItemTableAdapter.InsertProjectTechPayItem(intProjectID, blnIsConstruction, strProjectType, intEmployeeID, intWarehouseID, intTechPayID, decTechPayPrice, intQuantity, decTotalTechPayPrice, DateTime.Now);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Insert Project Techpay Item " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public ProjectTechPayItemsDataSet GetProjectTechPayItemsInfo()
+        {
+            try
+            {
+                aProjectTechPayItemsDataSet = new ProjectTechPayItemsDataSet();
+                aProjectTechPayItemsTableAdapter = new ProjectTechPayItemsDataSetTableAdapters.projecttechpayitemsTableAdapter();
+                aProjectTechPayItemsTableAdapter.Fill(aProjectTechPayItemsDataSet.projecttechpayitems);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Get Project Tech Pay Items Info " + Ex.Message);
+            }
+
+            return aProjectTechPayItemsDataSet;
+        }
+        public void UpdateProjectTechPayItemsDB(ProjectTechPayItemsDataSet aProjectTechPayItemsDataSet)
+        {
+            try
+            {
+                aProjectTechPayItemsTableAdapter = new ProjectTechPayItemsDataSetTableAdapters.projecttechpayitemsTableAdapter();
+                aProjectTechPayItemsTableAdapter.Update(aProjectTechPayItemsDataSet.projecttechpayitems);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Tech Pay Class // Update Project Tech Pay Items Update " + Ex.Message);
+            }
+        }
         public FindTechPayItemByDescriptionDataSet FindTechPayItemByDescription(string strJobDescription)
         {
             try
